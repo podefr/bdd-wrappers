2d9ae127fea3e797b845a80e4009c66da4cb6565
Olivier Scherrer <pode.fr@gmail.com>
2013-12-28 18:17:52 -0500

    add use strict and allow to work in browser

--- bdd-wrappers.js
+++ bdd-wrappers.js
@@ -3,15 +3,17 @@
  * Copyright(c) 2013 Olivier Scherrer <pode.fr@gmail.com>
  * MIT Licensed
  */
+'use strict';
+
 function wrap(name, step) {
 	return function func(desc, cb) {
 		step(name + desc, cb);
 	};
 }
 
-GLOBAL.GIVEN = wrap("GIVEN ", describe);
-GLOBAL.WHEN = wrap("WHEN ", describe);
-GLOBAL.AND = wrap("AND ", describe);
-GLOBAL.THEN = wrap("IT ", it);
+this.GIVEN = wrap("GIVEN ", describe);
+this.WHEN = wrap("WHEN ", describe);
+this.AND = wrap("AND ", describe);
+this.THEN = wrap("IT ", it);
 
 module.exports.wrap = wrap;
